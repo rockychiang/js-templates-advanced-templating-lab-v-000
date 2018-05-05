e1ac0e83ac598a2ca43d3d989d1b70fc774d2f7f
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 05:07:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial')
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
