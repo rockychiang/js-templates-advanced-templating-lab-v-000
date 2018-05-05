8d77b84d7ed9457bf4619c8af03b5cc8b2690dd5
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 05:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   
+  Handlebars.registerHelper()
+  
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
