d72864d4961c78850b2546c73801ec728524563b
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 06:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ function init() {
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
-  
-
-  
 }
 
 function displayEditForm() {
