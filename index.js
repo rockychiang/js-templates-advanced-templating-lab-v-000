5e6641969f7089db5213c246157f1c5061e29cc6
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 05:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ function init() {
     return new Handlebars.SafeString(ingredient)
   })
   
-  function displayEditForm(){
-    
+  function displayEditForm() {
+    var name = document.getElementById("nameHeader").innerText
+    var description = document.getElementById("recipeDescription").innerText
+    var ingredientsNodes = document.getElementsByName("ingredientsList")
+    var ingredients = []
+    for(var i=0;i<ingredientsNodes.length;i++) {
+      ingredients.push(ingredientsNodes[i].innerText)
+    }
+  
+    var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
+  
+    var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
+    var template = Handlebars.compile(recipeFormTemplate)
+    document.getElementById("main").innerHTML = template(recipe)
   }
   
   function createRecipe() {
@@ -40,21 +52,7 @@ function initForm() {
 
 
 
-function displayEditForm() {
-  var name = document.getElementById("nameHeader").innerText
-  var description = document.getElementById("recipeDescription").innerText
-  var ingredientsNodes = document.getElementsByName("ingredientsList")
-  var ingredients = []
-  for(var i=0;i<ingredientsNodes.length;i++) {
-    ingredients.push(ingredientsNodes[i].innerText)
-  }
 
-  var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
-
-  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
-  var template = Handlebars.compile(recipeFormTemplate)
-  document.getElementById("main").innerHTML = template(recipe)
-}
 
 function getRecipeVals() {
   var ingredientsNodes = document.getElementsByName("ingredients")
