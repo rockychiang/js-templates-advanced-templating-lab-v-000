cd7ef24a67f5380527cb12036b93ba7c97c569fe
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 06:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,19 +27,6 @@ function init() {
     document.getElementById("main").innerHTML = template(recipe)
   }
   
-  function getRecipeVals() {
-    var ingredientsNodes = document.getElementsByName("ingredients")
-    var ingredients = []
-    for(var i=0;i<ingredientsNodes.length;i++) {
-      if(ingredientsNodes[i].value !== "") {
-        ingredients.push(ingredientsNodes[i].value)
-      }
-    }
-    var name = document.getElementById("name").value
-    var description = document.getElementById("description").value
-    var recipe = {name, ingredients, description}
-    return(recipe)
-  }
 }
 
 function createRecipe() {
@@ -54,6 +41,20 @@ function updateRecipe() {
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
   var template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
+}
+
+function getRecipeVals() {
+  var ingredientsNodes = document.getElementsByName("ingredients")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    if(ingredientsNodes[i].value !== "") {
+      ingredients.push(ingredientsNodes[i].value)
+    }
+  }
+  var name = document.getElementById("name").value
+  var description = document.getElementById("description").value
+  var recipe = {name, ingredients, description}
+  return(recipe)
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
