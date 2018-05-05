00f25ab032b383f4af8e2d7bae6d6fc2277a0513
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 05:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,4 +30,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
   })
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
