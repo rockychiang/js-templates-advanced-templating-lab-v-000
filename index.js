486f62b6450b34bc1513005b98a6afff69b4a980
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 07:03:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function displayEditForm() {
 
 function createRecipe() {
   var recipe = getValues()
-  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+  const template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
   
   document.getElementById("main").innerHTML = template(recipe)
 }
