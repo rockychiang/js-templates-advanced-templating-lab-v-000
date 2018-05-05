8b47d4fe8795bfe027458c7bf648a27b952260ed
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 06:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function init() {
     return new Handlebars.SafeString(ingredient)
   })
   
-  var formTemplate = document.getElementById("recipe-form-template").innerHTML
-  var template = Handlebars.compile(formTemplate)
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
